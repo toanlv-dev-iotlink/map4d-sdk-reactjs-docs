d44e41619fff7f2eae22fb596729c853a7a07a42
Lê Văn Toàn <vantoan2509@gmail.com>
2022-12-07 18:06:29 +0700

    first build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  assetPrefix: 'https://docs.map4d.vn/map4d-map/react-js/v1.0',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/map4d-map/react-js/v1.0'
+})
