260416f50a16dc9dd103ddacca2c0e211cec0418
Lê Văn Toàn <vantoan2509@gmail.com>
2023-04-12 11:14:55 +0700

    continute

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withNextra = require("nextra")({
 const { PHASE_EXPORT, PHASE_PRODUCTION_BUILD } = require("next/constants");
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase == PHASE_EXPORT || phase == PHASE_PRODUCTION_BUILD) {
+  if (process.env.APP_ENV == "export") {
     return withNextra({
       assetPrefix: "https://docs.map4d.vn/map4d-map/react-js/v1.1.8",
       images: {
